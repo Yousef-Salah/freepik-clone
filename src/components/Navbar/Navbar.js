1703ca04352390f2af8bbfebbb986e94550bcd31
Omar M. Hawash <omarmhawash@Omars-MacBook-Air.local>
2022-11-02 22:15:51 +0200

    navbar --more content

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,31 +6,57 @@ const Navbar = () => {
         <div>
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="xnavbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler">
                         <span>
                             <img src={require('../../assets/images/burger-menu-toggle.png')} alt="burger-menu-toggle" />
                         </span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="collapse navbar-collapse">
                         <a href="x">
-                            <img src={require('../../assets/images/freepik-no_bg.png')} alt="logo" />
+                            <img className="nav-logo" src={require('../../assets/images/freepik-no_bg.png')} alt="logo" />
                         </a>
-                        <div className="nav-menu">
-                            <ul>
-                                <li><a href="https://www.freepik.com/popular-vectors">Vectors</a></li>
-                                <li><a href="https://www.freepik.com/popular-photos">Photos</a></li>
-                                <li><a href="https://www.freepik.com/popular-psd">PSD</a></li>
-                                <li>
-                                    <a href="https://www.freepik.com/popular-psd">3D</a>
-                                    <button className="btn">NEW</button>
-                                </li>
-                                <li><a href="https://www.freepik.com/popular-psd">Fonts</a></li>
-                                <li><a href="https://www.freepik.com/popular-psd">Categories</a></li>
-                                <li><a href="https://www.freepik.com/popular-psd">Collections</a></li>
-                                <li><a href="https://www.freepik.com/popular-psd">More</a></li>
-                            </ul>
+                        <div className="nav-menus">
+
+                            <div className="nav-menu">
+                                <ul>
+                                    <li><a href="https://www.freepik.com/popular-vectors">Vectors</a></li>
+                                    <li><a href="https://www.freepik.com/popular-photos">Photos</a></li>
+                                    <li><a href="https://www.freepik.com/popular-psd">PSD</a></li>
+                                    <li>
+                                        <a href="https://www.freepik.com/popular-psd">3D</a>
+                                        <button className="btn">NEW</button>
+                                    </li>
+                                    <li><a href="https://www.freepik.com/popular-psd">Fonts</a></li>
+                                    <li>
+                                        <a href="https://www.freepik.com/popular-psd">Categories
+                                            <i className="bi bi-caret-down-fill"></i>
+                                        </a>
+                                    </li>
+                                    <li><a href="https://www.freepik.com/popular-psd">Collections</a></li>
+                                    <li><a href="https://www.freepik.com/popular-psd">More
+                                        <i className="bi bi-caret-down-fill"></i>
+                                    </a></li>
+                                </ul>
+                            </div>
+                            <div className="pricing-nav">
+                                <ul>
+                                    <li><a href='https://www.freepik.com/popular-psd'>Sell Content</a></li>
+                                    <li><a className="pricing-color" href='https://www.freepik.com/popular-psd'>Pricing</a></li>
+                                </ul>
+
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div className="nav-menu-user">
+                    <ul>
+                        <li>
+                            <a className='login-link' href="https://www.freepik.com/popular-psd">Log in </a>
+                        </li>
+                        <li>
+                            <button className="signup-btn">Sign Up</button>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </div>
