466f29309d3c3c46db2822a863011619d6d7d49e
Ghassanmaq <ghassanmaq7@gmail.com>
2022-11-03 22:40:54 +0200

    Offcanvas Demo`

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,17 +1,43 @@
 import { click } from "@testing-library/user-event/dist/click";
-
+import './Filters.css';
+import FiltersBar from "./FiltersBar";
 const Filters=()=>{
 
     return(
+<div>
       <div className='filters'>
         <div className="container-fluid">
-        <button className="btn  btn-light filtersbtn" type="button" id="filters"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-sliders" viewBox="0 0 16 16">
+        <button className="btn  btn-light filtersbtn" type="button" id="filters" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-sliders" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M11.5 2a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM9.05 3a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0V3h9.05zM4.5 7a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM2.05 8a2.5 2.5 0 0 1 4.9 0H16v1H6.95a2.5 2.5 0 0 1-4.9 0H0V8h2.05zm9.45 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-2.45 1a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0v-1h9.05z"/>
 </svg>
 Filters</button>
+<div className="SideBar container-fluid">
+
+<div className="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+  <div className="offcanvas-header">
+    <h5 className="offcanvas-title" id="offcanvasScrollingLabel"><ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul></h5>
+    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div className="offcanvas-body">
+  <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+  </div>
 </div>
-<div className="row">
-<p>Category</p>
+</div>
+</div>
+
 </div>
 </div>
     );
---
+++ src/components/FiltersBar.js
@@ -0,0 +1,7 @@
+const FiltersBar=()=>{
+    return(
+        
+
+    )
+}
+export default FiltersBar
