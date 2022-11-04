c904b06dfb8834b898012b77a9e6d2f4e11ae3f5
Ahmad Thaher <ahmad.thaher2003@gmail.com>
2022-11-04 20:20:17 +0200

    Card.js doing the photo and the pragraph

---
+++ src/components/cards/Crads.js
@@ -0,0 +1,21 @@
+import './Cards.css' 
+
+
+const CardHeader=  () =>
+{
+    return (
+    
+        <div className="row">
+            <div className="leftPhoto">
+                <img src="pnada.jpg" alt="not found"></img>
+            </div>
+            <div className="rightPargraph">
+                <div className="content">
+                    <h3>Promote children's films and TV shows with cartoony assets</h3>
+                    <p> lorem </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default CardHeader
