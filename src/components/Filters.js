b367d472001297a56305694e9915e48fcfad795c
Ghassanmaq <ghassanmaq7@gmail.com>
2022-11-04 20:49:09 +0200

    collapse

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,6 +1,8 @@
 import { click } from "@testing-library/user-event/dist/click";
+import CollapseBtn from "./CollapseBtn";
 import './Filters.css';
 import FiltersBar from "./FiltersBar";
+import Tag from "./Tag";
 const Filters=()=>{
 
     return(
@@ -23,16 +25,16 @@ Filters</button>
     <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div className="offcanvas-body">
-  <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
+  <p>
+  <CollapseBtn target="#categoryCollapse" aria="categryCollapse" pathd="M3 4.5h10a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1H3zM1 2a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 2zm0 12a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 14z" title="Categroy"/>
+</p>
+<div className="collapse" id="categoryCollapse">
+<Tag title="Vectors" className="sidetag"/> 
+<Tag title="Photos"  className="sidetag"/> 
+<Tag title="PSD"  className="sidetag"/> 
+<Tag title="Icons"  className="sidetag"/> 
+</div>
+<CollapseBtn title="License" pathd="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2ZM1 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4Zm7.5.5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7ZM2 5.5a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1H2.5a.5.5 0 0 1-.5-.5Zm0 2a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1H2.5a.5.5 0 0 1-.5-.5Zm0 2a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1H2.5a.5.5 0 0 1-.5-.5ZM10.5 5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3ZM13 8h-2V6h2v2Z" />
   </div>
 </div>
 </div>
