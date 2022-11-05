095a74df29e09631068d932921790533ba7d58e0
Ahmad Thaher <ahmad.thaher2003@gmail.com>
2022-11-05 12:41:39 +0200

    using css page to desgin the cards and responsive it

--- src/components/cards/Crads.js
+++ src/components/cards/Crads.js
@@ -12,7 +12,10 @@ const CardHeader=  () =>
             <div className="rightPargraph">
                 <div className="content">
                     <h3>Promote children's films and TV shows with cartoony assets</h3>
-                    <p> lorem </p>
+                    <p> Products aimed at younger audiences require designs that are especifically thought for them.
+                         It's no surprise that toys, posters for children's films or TV series
+                        , and other producs like these resort to a more cartoony style
+                        , since it resonates very well with such an audience. Grab their attention and you'll also pique their parents' interest. </p>
                 </div>
             </div>
         </div>
