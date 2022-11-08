aa316c6c7ee553133bf8b27c95ced6f824209c28
Yousef.Salah <khodrojy3@gmail.com>
2022-11-08 22:39:32 +0200

    basic structure of SponsoredBy component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SearchInput from "./components/SearchInput";
+import SponsoredBy from "./components/SponsoredBy";
 
 function App() {
   return (
-    <div className="container-fluid mt-5">
-      <div className="row">
-        <div className="col-12 align-item-center mr-auto ml-auto">
-          <SearchInput  className="yousef salah khodroj"/>
+    <div>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <div className="col-12 align-item-center mr-auto ml-auto">
+            <SearchInput  className="yousef salah khodroj"/>
+          </div>
         </div>
       </div>
+      <SponsoredBy className="mt-5"/>
     </div>
   );
 }
---
+++ src/components/SponsoredBy.js
@@ -0,0 +1,46 @@
+import "./SponsoredBy.css";
+
+const SponsoredBy = (props) => {
+  const classes = (props.className) ? props.className : "";
+
+  return (
+    <div className={"container rounded " + classes} id={"sponsored-by-component " + props.id}>
+      <section className="row">
+        <article className="col-3 overflow-hidden">
+          <a href="https://stock.adobe.com/images/id/539813006?get_facets=1&order=relevance&safe_search=1&filters%5Bcontent_type%3Azip_vector%5D=1&clickref=1101lwhJdesG&mv=affiliate&mv2=Freepik&as_camptype=&as_channel=affiliate&as_source=partnerize&as_campaign=Freepik&as_content=api&as_audience=srp&sdid=6WTV6YJ5&asset_id=539813006">
+            <img
+              src="https://t3.ftcdn.net/jpg/05/39/81/30/240_F_539813006_5ZJhMgukoWJf8UAJVy45kPSCJs89ZYVY.jpg"
+              alt=""
+            />
+          </a>
+        </article>
+        <article className="col-3 overflow-hidden">
+          <a href="https://stock.adobe.com/images/id/539813006?get_facets=1&order=relevance&safe_search=1&filters%5Bcontent_type%3Azip_vector%5D=1&clickref=1101lwhJdesG&mv=affiliate&mv2=Freepik&as_camptype=&as_channel=affiliate&as_source=partnerize&as_campaign=Freepik&as_content=api&as_audience=srp&sdid=6WTV6YJ5&asset_id=539813006">
+            <img
+              src="https://t4.ftcdn.net/jpg/05/37/89/69/240_F_537896903_CnboBEBXJ4FFbUeXJOnFAFVMTQaycQGI.jpg"
+              alt=""
+            />
+          </a>
+        </article>
+        <article className="col-3 overflow-hidden">
+          <a href="https://stock.adobe.com/images/id/539813006?get_facets=1&order=relevance&safe_search=1&filters%5Bcontent_type%3Azip_vector%5D=1&clickref=1101lwhJdesG&mv=affiliate&mv2=Freepik&as_camptype=&as_channel=affiliate&as_source=partnerize&as_campaign=Freepik&as_content=api&as_audience=srp&sdid=6WTV6YJ5&asset_id=539813006">
+            <img
+              src="https://t3.ftcdn.net/jpg/05/40/06/16/240_F_540061602_IlVgsws0IqhHcpxlpOghqsEh6LO0fCx1.jpg"
+              alt=""
+            />
+          </a>
+        </article>
+        <article className="col-3 overflow-hidden">
+          <a href="https://stock.adobe.com/images/id/539813006?get_facets=1&order=relevance&safe_search=1&filters%5Bcontent_type%3Azip_vector%5D=1&clickref=1101lwhJdesG&mv=affiliate&mv2=Freepik&as_camptype=&as_channel=affiliate&as_source=partnerize&as_campaign=Freepik&as_content=api&as_audience=srp&sdid=6WTV6YJ5&asset_id=539813006">
+            <img
+              src="https://t4.ftcdn.net/jpg/05/36/81/01/240_F_536810188_O6akZ1GmtxWx0jySH7dXq9BVnQmYNjVq.jpg"
+              alt=""
+            />
+          </a>
+        </article>
+      </section>
+    </div>
+  );
+};
+
+export default SponsoredBy;
