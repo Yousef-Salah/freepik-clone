a616309487423db7b89235ec7f0ef3cec723547d
Sameera Najjar <sameera.najjar14@gmail.com>
2022-11-10 12:40:22 +0200

    image card component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 //import logo from './logo.svg';
 import './App.css';
-import Photo from './component/Joins';
-//<Photo/>
-//import Imag from './component/Imag';
-
+//import Photo from './component/Joins';
+// <Photo/>
+import Imag from './component/Imag';
+//
 function App() {
   return (
     <div>
-      <Photo/>
-      
+    
+     <Imag/>
+    
     </div>
   );
 }
--- src/component/Imag.js
+++ src/component/Imag.js
@@ -1,53 +1,43 @@
- import vector from "./images/Internet-Vector.jpg"
- import lllustrations from "./images/card-1.jpg"
- import photo from "./images/photo.jpg"
- import template from "./images/templates.jpg"
- import font from "./images/Fonts.jpg"
- import mockup from "./images/Mockup.jpg"
- import background from "./images/background.jpg"
- import textEffect from "./images/text-effect.jpg"
- import'./ImageCard.css';
 
- const ImageCard= () =>
+ import'./ImageCard.css';
+const ImageCard= () =>
 {
-  return(
-    
+return(
 <div className="row">
 <div className="column">
-  
-<img src={vector} alt="Vectors" ></img>
+<img src="./images/Internet-Vector.jpg" alt="Vectors" ></img>
 <p className="text" >Vectors</p>
-</div>
-<div className="column">
-<img src={lllustrations} alt="lllustrations" ></img>
-  <p className="text" >lllustrations</p>
-</div>
-<div className="column">
-<img src={photo} alt="Photos" ></img>
-  <p className="text" >Photos</p>
-</div>
-<div className="column">
-<img src={template} alt="Templates" ></img>
-  <p className="text" >Templates</p>
-</div>
-<div className="column">
-<img src={font} alt="Fonts" ></img>
-  <p className="text" >Fonts</p>
-</div>
-<div className="column">
-<img src={mockup} alt="Mockaups" ></img>
-  <p className="text" >Mockaups</p>
-</div>
-<div className="column">
-<img src={background} alt="Backgrounds" ></img>
-  <p className="text" >Backgrounds</p>
-</div>
-<div className="column">
-<img src={textEffect} alt="TextEffects" ></img>
-  <p className="text" >TextEffects</p>
-</div>
-</div>
-  );
-}
 
-export default ImageCard
+</div>
+<div className="column">
+<img src="./images/card-1.jpg" alt="lllustrations" ></img>
+<p className="text" >lllustrations</p>.
+</div>
+<div className="column">
+<img src="./images/Photos.jpg" alt="Photos" ></img>
+<p className="text" >Photos</p>
+</div>
+<div className="column">
+<img src="./images/templates.jpg" alt="Templates" ></img>
+<p className="text" >Templates</p>
+</div>
+<div className="column">
+<img src="./images/Fonts.jpg" alt="Fonts" ></img>
+<p className="text" >Fonts</p>
+</div>
+<div className="column">
+<img src="./images/Mockup.jpg" alt="Mockaups" ></img>
+<p className="text" >Mockaups</p>
+</div>
+<div className="column">
+<img src="./images/background.jpg" alt="Backgrounds" ></img>
+<p className="text" >Backgrounds</p>
+</div>
+<div className="column">
+<img src="./images/text-effect.jpg" alt="TextEffects" ></img>
+<p className="text" >TextEffects</p>
+</div>
+</div>
+);
+}
+export default ImageCard;
