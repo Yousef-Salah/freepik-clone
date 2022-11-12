6b4f27296492c5d2f2565f0ede0513ca4707e898
Ahmad Thaher <ahmad.thaher2003@gmail.com>
2022-11-13 01:25:24 +0200

    the header with 2 cadr photo and text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import CardHeader from './components/cards/Crads';
+import HeaderWithPargraph from './components/headercato/HeaderCato';
 
 
 function App() {
   return (
+    <>
+
 <CardHeader/>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<HeaderWithPargraph isRight={true}/>
+
+</>
   )
 }
 
---
+++ src/components/headercato/HeaderCato.js
@@ -0,0 +1,22 @@
+import './HeaderCato.css'
+const HeaderWithPargraph=  () =>
+{
+    return(
+     <div className='container'>
+      <div className='row' >
+      <div className='col col-lg-4 col-md-6 col-sm-12 leftImageCard'>
+        <img src='leftimg.png' alt='...' ></img>
+      </div>
+      <div className='col col-lg-4 col-md-6 col-sm-12'>
+        <h1 className='title'>Cartoons are a way to give life to your creations</h1>
+        <p className='descreption'>People have been drawing cartoons for quite some time now. This style has evolved over time and it's now more than a humorous depiction of someone. It's very popular among children, and an interesting way of showing an image of yourself to others. It has a colorful nature and a different way of interpreting things from reality.</p>
+      </div>
+      <div className='col col-lg-4 col-md-6 col-sm-12 rightImageCard'>
+        <img src='rightimg.png' alt='...' ></img>
+      </div>
+      </div>
+     </div>
+    );
+}
+
+export default HeaderWithPargraph;
