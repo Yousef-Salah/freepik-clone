ae910efaa2677eca2a828bbc1d77efe8c3b3d3b7
Ghassanmaq <ghassanmaq7@gmail.com>
2022-11-16 12:12:35 +0200

    fixes.. again

--- src/components/Sidebar/SideBar.js
+++ src/components/Sidebar/SideBar.js
@@ -343,8 +343,8 @@ const SideBar = () => {
                     <div className="absolute right-0 w-11 h-6 bg-gray-200 peer-focus:outline-none  dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-white-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                   </label>
                   <p className="collapsep">
-                    Edit online with <a href="/">Wepik</a> or
-                    <br /> <a href="/">Storyset</a>
+                    Edit online with <a className="link" href="/">Wepik</a> or
+                    <br /> <a className="link" href="/">Storyset</a>
                   </p>
                 </div>
               </span>
