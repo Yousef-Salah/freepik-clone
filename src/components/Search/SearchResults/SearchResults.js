2c1ad23c50835258dc2afb97366428804be603e9
Omar M. Hawash <omarmhawash@Omars-MacBook-Air.local>
2022-11-30 20:25:46 +0200

    cleanining up comments

--- src/components/Search/SearchResults/SearchResults.js
+++ src/components/Search/SearchResults/SearchResults.js
@@ -1,26 +1,16 @@
 import React, { useState, useEffect } from 'react';
+
 import './search-results.scss';
 import FinalImages from '../../../utils/FinalImages.json';
-// import DataGenerator from '../../../utils/DataGenerator';
 const SearchResults = () => {
-    // let data = DataGenerator();
     const [images, setImages] = useState(FinalImages);
     const [filteredImages, setFilteredImages] = useState([]);
-    // console.log(images);
 
     useEffect(() => {
         // console.log(images);
-        // let _filteredImages = images.filter(image => Date.parse(image.publish_date) > Date.parse('10-10-2021'));
-        let _filteredImages = images.filter(image => image.title.toLowerCase().includes('background'));
-        // let _filteredImages = images.filter(image => image.title.toLowerCase().includes('water'));
-        // let _filteredImages = images.filter(image => image.age === true);
-        // let _filteredImages = images.filter(image => image.age.toLowerCase() === "child");
 
-        // //* map imgDataSet to images
-        // let _filteredImages2 = imgDataSet.map(image => {
-        //     let _imgDataSet = images.find(img => img.id === image.id);
-        //     return { ...image, ..._imgDataSet };
-        // });
+        let _filteredImages = images.filter(image => image.title.toLowerCase().includes('background'));
+
         setFilteredImages(_filteredImages);
     }, [])
 
@@ -31,15 +21,15 @@ const SearchResults = () => {
     }, [filteredImages])
 
     return (
-        <div className="xyz">
+        <div>
+            {/* only for testing data*/}
             {filteredImages.map(item => {
                 return (
-                    <div class="test-123" key={item.id}>
+                    <div key={item.id}>
                         <h4>{item.title}</h4>
                         <h5>{item.category}</h5>
                         <img src={item.img_thumb} alt={item.title + "-thumb"} />
                         <br />
-                        {/* comment this + uncomment scss to preview set together */}
                         <img src={item.img_og} alt={item.title} />
                     </div>
                 )
