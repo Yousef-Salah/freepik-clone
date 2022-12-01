de356b2ea7fd4749378e040f9304eca4f3e942a3
HalaKhalifa <114260603+HalaKhalifa@users.noreply.github.com>
2022-12-01 21:34:31 +0200

    Hala (#33)
    * add responsive
    
    * fix font in responsive
    
    * add array of object
    
    * Setting components to dynamic and add link
    
    * fix some mistakes after review
    
    * fix the dynamic issue and add search image card component
    
    Co-authored-by: AhmadThaher <116901707+AhmadThaher@users.noreply.github.com>

--- src/components/Home/TrendingCategories/TrendingCard.js
+++ src/components/Home/TrendingCategories/TrendingCard.js
@@ -1,9 +1,16 @@
-const TrendingCard = (props) => {
-  return (
+import { Link } from "react-router-dom";
+const TrendingCard = ({Trendingphoto}) => {
+  return(
     <div className="container" id="trend-content">
-      <div className="trend-zoom">
-        <img className="trendimg" src={require(`../../../${props.imglink}`)} alt={props.title}></img>
-        <p className="trendtext">{props.phototitle}</p>
+      <div className="trend-zoom" key={Trendingphoto.id}>
+        <Link to={Trendingphoto.weburl}>
+          <img
+            className="trendimg"
+            src={require(`../../../${Trendingphoto.image}`)}
+            alt={Trendingphoto.title}
+          />
+          <p className="trendtext">{Trendingphoto.title}</p>
+        </Link>
       </div>
     </div>
   );
