26715ee70253cdd5e00bf27965cd0a20f1ec6144
Yousef.Salah <khodrojy3@gmail.com>
2022-12-05 13:04:31 +0200

    Revert "fix last merge issues"

--- src/components/Home/TrendingCategories/TrendingCard.js
+++ src/components/Home/TrendingCategories/TrendingCard.js
@@ -1,18 +1,17 @@
 import { Link } from "react-router-dom";
 const TrendingCard = ({Trendingphoto}) => {
-  return(
     <div className="container" id="trend-content">
       <div className="trend-zoom" key={Trendingphoto.id}>
         <Link to={Trendingphoto.weburl}>
           <img
             className="trendimg"
-            src={require(`../../../${Trendingphoto.image}`)}
-            alt={Trendingphoto.title}
+            src={Trendingphoto.imglink}
+            alt={Trendingphoto.phototitle}
           />
-          <p className="trendtext">{Trendingphoto.title}</p>
+          <p className="trendtext">{Trendingphoto.phototitle}</p>
         </Link>
       </div>
     </div>
-  );
+
 };
 export default TrendingCard;
--- src/components/Home/TrendingCategories/TrendingCategories.js
+++ src/components/Home/TrendingCategories/TrendingCategories.js
@@ -3,8 +3,11 @@ import TrendingCard from "./TrendingCard";
 
 import Cardsphotos from "./Cardsphotos";
 const TrendingCategories = () => {
+<<<<<<< HEAD
   const data = Cardsphotos();
 
+=======
+>>>>>>> parent of ac77fa3 (fix some mistakes after review)
   return (
 
     <section className="container-fluid" id="trending">
@@ -15,12 +18,20 @@ const TrendingCategories = () => {
         Check what's popular on Freepik and make your project look trendy and
         professional.
       </p>
-
       <div className="grid" id="exploretrend">
-        {data.map((Trendingphoto) => {
+        {Cardsphotos.map((Trendingphoto) => {
           return (
             <div className="item" id={Trendingphoto.id}>
+<<<<<<< HEAD
               <TrendingCard Trendingphoto={Trendingphoto}/>
+=======
+              <TrendingCard
+                id={Trendingphoto.id}
+                imglink={Trendingphoto.imglink}
+                phototitle={Trendingphoto.phototitle}
+                weburl={Trendingphoto.weburl}
+              />
+>>>>>>> parent of ac77fa3 (fix some mistakes after review)
             </div>
             
           );
