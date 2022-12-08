ffbe20ca6fa6c5e9d0ec3d4ec6b18a2c3565aa69
Yousef.Salah <khodrojy3@gmail.com>
2022-12-08 12:44:32 +0200

    delete unnecessary folder

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ import SearchContainer from '../components/Search/SearchBox/SearchContainer';
 import Annual from "../components/Home/AnnualDiscount/AnnualDiscount";
 import Trending from "../components/Home/TrendingCategories/TrendingCategories";
 import CircleCategoy from "../components/Home/CircleCategoy/CircleCategoy";
-import InnerCreation from "../components/Home/InnerCreation/InnerCreation";
 import JoinSection from "../components/Common/JoinSection/JoinSection";
 import Promote from '../components/Home/PromoteSiteFeatures/Promote';
 import CreatorCollection from '../components/Home/CreatorCollection/CreatorCollection';
@@ -23,7 +22,6 @@ const Home = (props) => {
             <Annual />
             <Trending />
             <CreatorCollection/>
-            <InnerCreation />
             <Promote/>
             <JoinSection />
             
