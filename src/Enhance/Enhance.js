e278abb665e26d0b33626d0269b11938508b2374
Sameera Najjar <sameera.najjar14@gmail.com>
2022-12-15 00:01:43 +0200

    Enhance image

--- src/Enhance/Enhance.js
+++ src/Enhance/Enhance.js
@@ -3,14 +3,29 @@ import './Enhance.css';
 import license from './Enhance/license.svg';
 import production from './Enhance/production.svg';
 import ads from './Enhance/ads.svg';*/
+import file from '../Enhance/index.1.jpg';
+import crown from '../Enhance/index.2.jpg';
+import refresh from '../Enhance/index3.jpg';
+import add from '../Enhance/index4.jpg';
 const Enhance = () => {
     return (
-      <div className="row row-cols-1 row-cols-md-4 "id="Enhance-container">
+      /*<div className='Enhance'>
+      <div className='EnhanceCard'>
+        <img src={file} className="Enhance-img"></img>
+        <h5 className='EnhanceTitle'>Full license</h5>
+        <p className='EnhanceText'>You can use any asset for commercial or personal use without attributing the author. High-quality photos, fonts, vector icons, stickers, and logos ready to use.</p>
+        </div>
+      
+       <div className='EnhanceCard'>
+       <img src={file} className="Enhance-img"></img>
+       <h5 className='EnhanceTitle'>Full license</h5>
+       <p className='EnhanceText'>You can use any asset for commercial or personal use without attributing the author. High-quality photos, fonts, vector icons, stickers, and logos ready to use.</p>
+       </div>
+       </div>*/
+     <div className="row row-cols-1 row-cols-md-4 "id="Enhance-container">
   <div className="col">
-    <div className="card">
-    <i className="fa-duotone fa-file"id="EnhanceSymbol"></i>
-   
-   
+    <div className="enhance">
+    <img src={file} class="card-img-top"id="Enhance-img" alt="file"></img>
       <div className="card-body">
         <h5 className="card-title"id="symbolTitle">Full license</h5>
         <p className="card-text"id="symbolText">You can use any asset for commercial or personal use without attributing the author. High-quality photos, fonts, vector icons, stickers, and logos ready to use.</p>
@@ -18,8 +33,8 @@ const Enhance = () => {
     </div>
   </div>
   <div className="col">
-    <div className="card">
-    <i className="fa-light fa-crown"id="EnhanceSymbol"></i>
+    <div className="enhance">
+    <img src={crown} class="card-img-top"id="Enhance-img" alt="crown"></img>
       <div className="card-body">
         <h5 className="card-title"id="symbolTitle">Premium content</h5>
         <p className="card-text"id="symbolText">Access to Freepik and Flaticon Premium content. Download exclusive editable images and more than 7.7MM vector icons, stickers, and logos.</p>
@@ -27,8 +42,8 @@ const Enhance = () => {
     </div>
   </div>
   <div className="col">
-    <div className="card">
-    <i className="fa-regular fa-repeat"id="EnhanceSymbol"></i>
+    <div className="enhance">
+    <img src={refresh} class="card-img-top"id="Enhance-img" alt="refresh"></img>
       <div className="card-body">
         <h5 className="card-title"id="symbolTitle">New assets daily</h5>
         <p className="card-text"id="symbolText">Discover over 50K new editable assets daily carefully curated. That means 250K photos, fonts, icons, stickers, and logos per week. More than 1.25MM assets per month.</p>
@@ -36,8 +51,8 @@ const Enhance = () => {
     </div>
   </div>
   <div className="col">
-    <div className="card">
-    <i className="fa-light fa-bullhorn"id="EnhanceSymbol"></i>
+    <div className="enhance">
+    <img src={add} class="card-img-top"id="Enhance-img" alt="add"></img>
       <div className="card-body">
         <h5 className="card-title"id="symbolTitle">Ad free</h5>
         <p className="card-text"id="symbolText">Focus on what matters with an ad-free interface and improve your browsing experience. Scroll through a screen of high-quality assets and select your best fit.</p>
