abe5ecc78a0cc92b9ebbab21428d8fd6f33a21bc
Sameera Najjar <sameera.najjar14@gmail.com>
2022-12-17 22:47:30 +0200

    make dynamic and responsive promote

---
+++ src/components/Home/PromoteSiteFeatures/PromoteImg.js
@@ -0,0 +1,11 @@
+import './Promote.css';
+import photo from "./resources.jpg";
+const PromoteImg = () =>{
+    return(
+        <div className="PromotePhoto">
+        <img src={photo} alt="promote" loading="lazy" ></img>
+</div>
+        
+    );
+}
+export default PromoteImg;
