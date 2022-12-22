aa40acb7f36cf9dbf236008d252540dc6ddf3b20
Ahmed Adel <engahmadadel04@gmail.com>
2022-12-22 20:14:50 +0200

    edit name func in css

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PricingSection from '../components/Pricing/PricingSection/PricingSection';
 //import Enhance from './components/Pricing/PricingSection/Enhance/Enhance';
 import Enhance from '../Enhance/Enhance';
+
 const Pricing = (props) => {
 
     //useEffect(() => {props.page("pricing")}, [])
