7fe6ee4ee13fb88f860f925009aa9037e3249d57
Ghassanmaq <ghassanmaq7@gmail.com>
2022-12-24 19:27:44 +0200

    removed data from search& fonts pages and fixed class naming p2

---
+++ src/utils/TagBarData3D.js
@@ -0,0 +1,19 @@
+const TagBarData3D=[{
+    title: "Photos",
+  pic: "bi bi-image tw-left-0",
+  className: "type-class",
+  className1: "type",page:'search'
+},
+{
+  title: "PSD",
+  pic: "bi bi-filetype-psd",
+  className: "type-class",
+  className1: "type",page:'search'
+},
+{
+  title: "All images",
+  pic: "bx bxs-shapes",
+  className: "type-class",
+  className1: "type",page:'search'
+}]
+export default TagBarData3D
