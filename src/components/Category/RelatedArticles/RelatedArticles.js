014cea60ab576b51976af57315a43ea2d79c6199
Yousef.Salah <khodrojy3@gmail.com>
2023-01-11 19:30:49 +0200

    delete RelatedArticles component code

--- src/components/Category/RelatedArticles/RelatedArticles.js
+++ src/components/Category/RelatedArticles/RelatedArticles.js
@@ -2,20 +2,9 @@ import React, { useState, useEffect } from 'react';
 import './RelatedArticles.css';
 
 
-const Related = (props) => {
+const RelatedArticles = (props) => {
   return (
-
-
-    <div className="col" id='Related-col'>
-      <div className="card h-100">
-        <img src={props.image} className="card-img-top" alt="..." ></img>
-        <div className="card-body">
-          <h5 className="#Related-title"id='Related-title'>{props.title}</h5>
-          <p className="Related-text"id='Related-text'>{props.heder} </p>
-
-        </div>
-      </div>
-    </div>
+<></>
   );
 }
-export default Related;
+export default RelatedArticles;
