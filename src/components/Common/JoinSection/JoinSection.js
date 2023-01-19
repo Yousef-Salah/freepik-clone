15b64867349906af3567cfa65637ba1ed74d0073
Omar M. Hawash <omarmh@Omars-MacBook-Air.local>
2023-01-19 12:35:50 +0200

    joinCreator changes

--- src/components/Common/JoinSection/JoinSection.js
+++ src/components/Common/JoinSection/JoinSection.js
@@ -1,21 +1,31 @@
 import photo from "../../../assets/images/Join-section/community.png";
-import './join-section.css';
+import "./join-section.css";
 const Join = () => {
-    return (
-        <div className="container" id="join-section">
-            <div className="content">
-                <p className="title" >
-                    Join Freepik <strong>creator community</strong>
-                </p>
-                <p className="text">Behind every stock image there's a creative mind.You can also create content and sell it on Freepik</p>
-                <a href="#" className="button" target="_blank" rel="noopener noreferrer">
-                    Sell content
-                </a>
-            </div>
-            <div className="photo">
-                <img src={photo} alt="Join Freepik" loading="lazy"></img>
-            </div>
+  return (
+    <div className="container" id="join-section">
+      <div className="content">
+        <p className="title">
+          Join Freepik's <strong>creator community</strong>
+        </p>
+        <p className="text">
+          Behind every stock image there's a creative mind.You can also create
+          content and sell it on Freepik
+        </p>
+        <a
+          href="#"
+          className="button"
+          target="_blank"
+          rel="noopener noreferrer">
+          Sell content
+        </a>
+      </div>
+      <div className="photo">
+        <div className="image-wrapper">
+          <img src={photo} alt="Join Freepik" loading="lazy"></img>
         </div>
-    );
-}
-export default Join
+      </div>
+    </div>
+  );
+};
+export default Join;
+
