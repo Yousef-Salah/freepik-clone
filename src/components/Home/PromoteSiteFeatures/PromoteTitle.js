4a63f84d274b8c2df696039f05005495420526e3
Omar M. Hawash <omarmh@Omars-MacBook-Air.local>
2023-01-20 22:11:32 +0200

    map function fixes

--- src/components/Home/PromoteSiteFeatures/PromoteTitle.js
+++ src/components/Home/PromoteSiteFeatures/PromoteTitle.js
@@ -1,12 +1,12 @@
 const PromoteTitle = () => {
   return (
     <div>
-      <p className="Promote-title">
+      <div className="Promote-title">
         <h3>
           The smartest choice for{" "}
           <strong className="strongTitle">creatives like you</strong>
         </h3>
-      </p>
+      </div>
       <p className="secondTitle">
         Whether you’re looking for designs or photographs, you’ll find the
         perfect asset on Freepik.
