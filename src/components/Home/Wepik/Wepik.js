b718e04cf370f05e51dee961070461b4c83f5a06
Omar M. Hawash <omarmh@Omars-MacBook-Air.local>
2023-01-22 06:40:05 +0200

    wepik component fixes

--- src/components/Home/Wepik/Wepik.js
+++ src/components/Home/Wepik/Wepik.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import './wepik.css';
+import React from "react";
+import "./wepik.css";
 const Wepik = (props) => {
-    return (
-        <div className="wepik-cont" id="wepik">
-            <div className="col-6 wepik">
-                <img className="wepik-logo " src={props.logoUrl} alt="wepik-logo" />
-                <h5 className="wepik-header" >
-                    {props.headerText}
-                </h5>
-                <h6 className="wepik-paragraph">
-                    {props.paragraphText}
-                </h6>
-                <button type="button" className="btn btn-primary btn-lg wepik-btn" >{props.buttonText}</button>
-            </div>
+  return (
+    <div className="wepik-cont" id="wepik">
+      <div className="col-6 wepik">
+        <img className="wepik-logo " src={props.logoUrl} alt="wepik-logo" />
+        <h5 className="wepik-header">{props.headerText}</h5>
+        <h6 className="wepik-paragraph">{props.paragraphText}</h6>
+        <button type="button" className="btn btn-primary btn-lg wepik-btn">
+          {props.buttonText}
+        </button>
+      </div>
 
-            <div className="col-6 d-sm-none d-none d-lg-block wepik-photo">
-                <img src={props.photoUrl} alt="wepik-pho" />
-            </div>
-        </div>
-    );
-}
+      <div className="col-6 d-lg-block wepik-photo">
+        <img src={props.photoUrl} alt="wepik-pho" />
+      </div>
+    </div>
+  );
+};
 
 export default Wepik;
+
