03311019b7c1894136b3909cb01c759c40652d42
OmarMHawash <omarmhawash@gmail.com>
2023-01-26 00:09:52 +0200

    general responsive fixes

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -2,12 +2,18 @@ import React from "react";
 import DiscoverCollectionTitle from "./DiscoverCollectionsTitle";
 import DiscoverCollections from "./DiscoverCollections";
 const Collection = () => {
-  return (
-    <>
-      <DiscoverCollectionTitle />
-      <DiscoverCollections />
-    </>
-  );
+	return (
+		<div id="discover-collection">
+			<div className="discover-list">
+				<DiscoverCollectionTitle />
+				<DiscoverCollections />
+			</div>
+			<div className="discover-list">
+				<DiscoverCollectionTitle />
+				<DiscoverCollections />
+			</div>
+		</div>
+	);
 };
 
 export default Collection;
