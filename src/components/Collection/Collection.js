606c1984a2ba0f04bd69e4c14110d7a933588c00
Omar M. Hawash <omarmh@Omars-MacBook-Air.local>
2023-01-25 17:25:32 +0200

    discover-collection rework

---
+++ src/components/Collection/Collection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DiscoverCollectionTitle from "./DiscoverCollectionsTitle";
+import DiscoverCollections from "./DiscoverCollections";
+const Collection = () => {
+  return (
+    <>
+      <DiscoverCollectionTitle />
+      <DiscoverCollections />
+    </>
+  );
+};
+
+export default Collection;
+
