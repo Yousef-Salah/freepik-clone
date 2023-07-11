ccb3da1d6a8249b54f94df86d79c236e2467be6b
Yousef.Salah <khodrojy3@gmail.com>
2023-07-11 04:45:35 +0300

    fixing eslint errors

--- src/components/Category/CategorySection/utils.jsx
+++ src/components/Category/CategorySection/utils.jsx
@@ -1,20 +1,19 @@
-const utils = () => {
-  return [
-    {
-      title: 'Get cool wallpapers and desktop backgrounds for your computer',
-      desc: 'Since we live in the computer era, devices are all around us. As we use them a lot, how about giving them your own personal touch? Backgrounds are easy to use, and they can convey what you feel, or just represent your favorite topics. You can use wonderful backgrounds for your cell phone, tablet or computer.',
-      img: 'https://fps.cdnpk.net/landings-paid/23022022/backgrounds/img-product-1.webp'
-    },
-    {
-      title: 'Add nice backgrounds to your slideshows and presentations',
-      desc: 'Whenever you need to pitch your ideas or show complex data in front of people, a PowerPoint presentation is a useful resource. Backgrounds will allow you to give each slide a better design, making it much easier to communicate with your audience and catch their interest. Give some context, add your personal touch or make the slides much more visually appealing.',
-      img: 'https://fps.cdnpk.net/landings-paid/23022022/backgrounds/img-product-2.webp'
-    },
-    {
-      title: 'Customize your own flyers and banners with backgrounds',
-      desc: "When designing banners or flyers or other kinds of promotional elements, in addition to the information that you need to show to people, a colorful background can help you catch the recipient's attention. Complete your design by adding a nice background so that it adapts to the composition that you've created.",
-      img: 'https://fps.cdnpk.net/landings-paid/23022022/backgrounds/img-product-3.webp'
-    }
-  ]
-}
-export default utils
+const categoryPageData = [
+  {
+    title: 'Get cool wallpapers and desktop backgrounds for your computer',
+    desc: 'Since we live in the computer era, devices are all around us. As we use them a lot, how about giving them your own personal touch? Backgrounds are easy to use, and they can convey what you feel, or just represent your favorite topics. You can use wonderful backgrounds for your cell phone, tablet or computer.',
+    img: 'https://fps.cdnpk.net/landings-paid/23022022/backgrounds/img-product-1.webp',
+  },
+  {
+    title: 'Add nice backgrounds to your slideshows and presentations',
+    desc: 'Whenever you need to pitch your ideas or show complex data in front of people, a PowerPoint presentation is a useful resource. Backgrounds will allow you to give each slide a better design, making it much easier to communicate with your audience and catch their interest. Give some context, add your personal touch or make the slides much more visually appealing.',
+    img: 'https://fps.cdnpk.net/landings-paid/23022022/backgrounds/img-product-2.webp',
+  },
+  {
+    title: 'Customize your own flyers and banners with backgrounds',
+    desc: "When designing banners or flyers or other kinds of promotional elements, in addition to the information that you need to show to people, a colorful background can help you catch the recipient's attention. Complete your design by adding a nice background so that it adapts to the composition that you've created.",
+    img: 'https://fps.cdnpk.net/landings-paid/23022022/backgrounds/img-product-3.webp',
+  },
+];
+
+export default categoryPageData;
